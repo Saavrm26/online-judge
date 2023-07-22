5a436b2ce4ec4ac99db35da7e198bf8bbf1eb074
saarthak <lci2021035@iiitl.ac.in>
2023-07-22 07:33:18 +0530

    nginx config changes

--- app/gunicorn_config/config.py
+++ app/gunicorn_config/config.py
@@ -1,6 +1,18 @@
+import socket
+
 wsgi_app = 'app.wsgi:application'
 loglevel = 'debug'
 workers = 2
 bind = '0.0.0.0:8000'
 # accesslog = errorlog = '/var/log/gunicorn.log'
 # capture_output = True
+forwarded_allow_ips = '127.0.0.1'
+
+
+def on_starting(server):
+    print("STARTING GUNICORN")
+    ip_nginx = socket.gethostbyname('nginx')
+    print(ip_nginx)
+    global forwarded_allow_ips
+    forwarded_allow_ips += f',{ip_nginx}'
+    print(forwarded_allow_ips)
